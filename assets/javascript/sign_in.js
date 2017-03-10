6bf4c28dc277d318edc92f23adff28d8f8e8f9bd
Paul Viola <Paul Viola>
2017-03-09 23:04:36 -0600

    started email log in

--- assets/javascript/sign_in.js
+++ assets/javascript/sign_in.js
@@ -7,8 +7,55 @@ $(document).ready(function(){
     storageBucket: "project-1-60d84.appspot.com",
     messagingSenderId: "1033595008210"
   };
+
   firebase.initializeApp(config);
   var database = firebase.database();
+//TODO:Need to add change for auth state with this function
+//TODO:need to remove User count and generate a seperate object for username and allow that to be used for logging in
+//TODO: With User UID should be possible to save picks off to databse by specific user.
+
+  /*firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+      $("#changingPanel").empty();
+        var additionForm = $("<form>");
+        additionForm.attr("id", "addForm");
+        $("#changingPanel").append(additionForm);
+
+        inputContent("trainName", "Train Name:", $("#addForm"));
+        inputContent("destination", "Destination:", $("#addForm"));
+        inputContent("firstTime", "First Train:", $("#addForm"));
+        inputContent("frequency", "Frequency:", $("#addForm"));
+
+        buttonGen("newTrainBtn", "Submit", $("#addForm"), "primary");
+        buttonGen("logOut", "Log Out", $("#addForm"), "secondary");
+    }
+  });*/
+
+ $(document).on("click", "#logInSubmit", e =>{
+  //btnLogIn.on("click", e => {
+    event.preventDefault();
+    const email = $("#emailInput").val().trim();
+    const password = $("#passwordInput").val();
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, password);
+
+    promise.catch(e => console.log(e.message));
+  });
+
+ $(document).on("click", "#newUserSubmit", e =>{
+ //btnSignUp.on("click", e => {
+   event.preventDefault();
+   const email = $("#emailInput").val().trim();
+   const password = $("#passwordInput").val();
+   const auth = firebase.auth();
+
+   const promise = auth.createUserWithEmailAndPassword(email, password);
+
+   promise.catch(e => console.log(e.message));
+ });
+
+
 
 //object contains all the functions needed to generate a panel
 var panelGen = {
@@ -41,7 +88,7 @@ var formGen = {
     return;
   },
   //creates a form group and attaches it to specified form
-  formGroup: function(inputId, formText, parentForm){
+  formGroup: function(inputId, formText, type, parentForm){
     var formGroup = $("<div>");
     formGroup.addClass("form-group");
 
@@ -52,7 +99,7 @@ var formGen = {
 
     var userInput = $("<input>");
     userInput.addClass("form-control");
-    userInput.attr("type", "text");
+    userInput.attr("type", type);
     userInput.attr("id", inputId);
     formGroup.append(userInput);
 
@@ -124,19 +171,34 @@ var tableGen ={
       userCount = snapshot.val().userCount;
   });
 
-  $("#signInBtn").on("click", function(){
+  $(document).on("click", "#signInBtn", function(){
     //prevents default and empties div to replace with sign in form
-    event.preventDefault();
+   event.preventDefault();
     $("#signInArea").empty();
 
     panelGen.createPanel("Please Sign In", "signInPanel", $("#signInArea"));
     formGen.createForm($("#signInPanel"), "signInForm");
-    formGen.formGroup("nameInput", "User Name", $("#signInForm"));
-    formGen.createSubmitBtn("nameSubmit", "Submit Name", $("#signInForm"));
+    formGen.formGroup("emailInput", "Email", "email", $("#signInForm"));
+    formGen.formGroup("passwordInput", "Password", "password", $("#signInForm"));
+    formGen.createSubmitBtn("logInSubmit", "Log In", $("#signInForm"));
 
   });
 
-  $(document).on("click", "#nameSubmit", function(){
+  $(document).on("click", "#signUpBtn", function(){
+    //prevents default and empties div to replace with sign in form
+   event.preventDefault();
+    $("#signInArea").empty();
+
+    panelGen.createPanel("Please Sign In", "signInPanel", $("#signInArea"));
+    formGen.createForm($("#signInPanel"), "signInForm");
+    formGen.formGroup("nameInput", "User Name", "text", $("#signInForm"));
+    formGen.formGroup("emailInput", "Email", "email", $("#signInForm"));
+    formGen.formGroup("passwordInput", "Password", "password", $("#signInForm"));
+    formGen.createSubmitBtn("newUserSubmit", "Submit", $("#signInForm"));
+
+  });
+
+  $(document).on("click", "#newUserSubmit", function(){
     event.preventDefault();
 
     userCount++
@@ -152,27 +214,53 @@ var tableGen ={
     newUser.userName = $("#nameInput").val().trim();
     //newUser.uid = userCount;
     $("#signInArea").empty();
-    database.ref("users/").push(newUser);
-
-  });
-
-  database.ref("users/").on("child_added", function(childSnapshot, prevChildKey){
     panelGen.createPanel("Top Players", "topPlayers" ,$("#signInArea"));
     tableGen.createTable("topTable", $("#topPlayers"));
     tableGen.tableHeadInitial("players", $("#topTable"));
     tableGen.tableHeaders("User Name", $("#players"));
     tableGen.tableBody("topBody", $("#topTable"));
+    database.ref("users/").push(newUser);
 
-    /*if(userCount > 5){
-      for(var i = 1; i <=5; i++){
-        tableGen.bodyAdd(childSnapshot.val().userName, $("#topBody"));
-      }
-    }
-    else{*/
-      tableGen.tableRow("playerName", $("#topBody"));
-      tableGen.rowAddition(childSnapshot.val().userName, $("#playerName"));
-  //  }
+  });
+  panelGen.createPanel("Top Players", "topPlayers" ,$("#signInArea"));
+  tableGen.createTable("topTable", $("#topPlayers"));
+  tableGen.tableHeadInitial("players", $("#topTable"));
+  tableGen.tableHeaders("User Name", $("#players"));
+  tableGen.tableBody("topBody", $("#topTable"));
+
+  database.ref("users/").on("child_added", function(snapshot){
+
+    /*  var sv = snapshot.val();
+      // Getting an array of each key In the snapshot object
+      var svArr = Object.keys(sv);
+      // Finding the last user's key
+      var lastIndex = svArr.length - 1;
+      var lastKey = svArr[lastIndex];
+      // Using the last user's key to access the last added user object
+      var lastObj = sv[lastKey]
+      // Console.loging the last user's data
+      console.log(lastObj.name);
+      console.log(lastObj.email);
+      console.log(lastObj.age);
+      console.log(lastObj.comment); */
+
+    var tableRow = $("<tr>");
+    var tableColumn = $("<td>");
+    tableColumn.html(snapshot.val().userName);
+    tableRow.append(tableColumn);
+    $("#topBody").append(tableRow);
     return;
   });
+  var logIn = $("<button>");
+  logIn.addClass("btn btn-primary btn-lg");
+  logIn.attr("id", "signInBtn");
+  logIn.html("Log In");
+  $("#topPlayers").append(logIn).append(" ");
+
+  var signUp = $("<button>");
+  signUp.addClass("btn btn-info btn-lg");
+  signUp.attr("id", "signUpBtn");
+  signUp.html("Sign Up");
+  $("#topPlayers").append(signUp);
 
 });
