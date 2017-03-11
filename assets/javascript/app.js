d3f93ea782d3040a272a03d3d4a96ba0f3745eef
donaldjkarr <donaldjkarr@gmail.com>
2017-03-11 13:35:03 -0600

    adjusted html and added fixture buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,17 +20,17 @@ $(document).ready(function(){
 
       //for each fixture prints into a table row before appending new row
       for(var i =0; i < matchArray.length; i++){
-      fixtureGen.addMatch(matchArray[i]);
+      fixtureGen.addMatch(matchArray[i], i );
       $("fixtures").append(matchRow);
       }
       return;
     },
 
     //2/2 functions that print fixtures
-    addMatch: function(arrayInput){
+    addMatch: function(arrayInput, data){
       matchRow = $("<tr>");
       var newFixture = $("<td>");
-      newFixture.html(arrayInput.homeTeamName + " vs " + arrayInput.awayTeamName);
+      newFixture.html("<span data-game="+ data +"-H>"+ arrayInput.homeTeamName +"</span> vs <span data-game="+ data +"-A>" + arrayInput.awayTeamName + "</span>");
       matchRow.append(newFixture);
       $("#fixtures").append(matchRow);
       return;
@@ -43,65 +43,91 @@ $(document).ready(function(){
     //1/3 functions used to create league standings table
     createStandings: function(tableResponse){
       //takes legue name from JSON object and prints it in table header
-			$("#leagueName").html(tableResponse.leagueCaption + "<br>Updated Through Matchday: " + tableResponse.matchday);
+      $("#leagueName").html(tableResponse.leagueCaption + "<br>Updated Through Matchday: " + tableResponse.matchday);
 
       //for each team in the JSON object, for loop adds each element("wins, team name, etc) to a table row and appends the row to the table")
-				for(var i=0; i < tableResponse.standing.length; i ++){
-					teamRow = $("<tr>");
+        for(var i=0; i < tableResponse.standing.length; i ++){
+          teamRow = $("<tr>");
 
-					tableGen.teamAdd(tableResponse.standing[i], "position");
-					tableGen.teamAdd(tableResponse.standing[i], "teamName");
-					tableGen.teamAdd(tableResponse.standing[i], "playedGames");
-					tableGen.AddWDL(tableResponse.standing[i], "wins", "draws", "losses");
-					tableGen.teamAdd(tableResponse.standing[i], "goalDifference");
-					tableGen.teamAdd(tableResponse.standing[i], "points");
+          tableGen.teamAdd(tableResponse.standing[i], "position");
+          tableGen.teamAdd(tableResponse.standing[i], "teamName");
+          tableGen.teamAdd(tableResponse.standing[i], "playedGames");
+          tableGen.AddWDL(tableResponse.standing[i], "wins", "draws", "losses");
+          tableGen.teamAdd(tableResponse.standing[i], "goalDifference");
+          tableGen.teamAdd(tableResponse.standing[i], "points");
 
-					 $("#teamInsert").append(teamRow);
-				}
-				return;
-		},
+           $("#teamInsert").append(teamRow);
+        }
+        return;
+    },
 
     //2/3 functions used to create league standings table
     //Adds a particular element to the table row. (first parameter takes the specific team, second is addition to the table)
-		teamAdd: function(team, addition){
-			var rowAddition = $("<td>");
-			rowAddition.html(team[addition]);
-			teamRow.append(rowAddition);
-			return;
-		},
+    teamAdd: function(team, addition){
+      var rowAddition = $("<td>");
+      rowAddition.html(team[addition]);
+      teamRow.append(rowAddition);
+      return;
+    },
 
     //3/3 function used to create league standings table
     //essentially the same as teamAdd(), has more parameters allowing all three parts of the object to be added to a single column of the table
-		AddWDL: function(team, wins, draws, losses){
-			var rowAddition = $("<td>");
-			rowAddition.html(team[wins] + "-" + team[draws] + "-" + team[losses]);
-			teamRow.append(rowAddition);
-			return;
-		}
+    AddWDL: function(team, wins, draws, losses){
+      var rowAddition = $("<td>");
+      rowAddition.html(team[wins] + "-" + team[draws] + "-" + team[losses]);
+      teamRow.append(rowAddition);
+      return;
+    }
+  }
+
+  var userPicks = {
+
+    printPicks: function(matchArray){
+      //takes matchDay from the JSOn object and prints it into header
+      $("#matchDay").html("Matchday: " + matchArray[0].matchday);
+
+      //for each fixture prints into a table row before appending new row
+      for(var i =0; i < matchArray.length; i++){
+      userPicks.addPick(matchArray[i]);
+      $("userpick").append(pickRow);
+      }
+      return;
+    },
+
+    addPick: function(arrayInput){
+      pickRow = $("<tr>");
+      var newPick = $("<td>");
+      newPick.html("<span>"+ arrayInput.homeTeamName + " <input type='radio'>  " + "</span>" + "<span>" + arrayInput.awayTeamName + " <input type='radio'>  " + "</span>" + "<span>" + "Draw" + " <input type='radio'>" + "<span>");
+      pickRow.append(newPick);
+      $("#userpick").append(pickRow);
+      return;
+    }
   }
 
 
-    //API call to obtain fixtures for a specified match day
-	$.ajax({
-	  headers: { 'X-Auth-Token': '183f8b1674a443d3b81e71fa06e8ac24' },
-	  url: 'http://api.football-data.org/v1/competitions/426/fixtures?matchday=27',
-	  dataType: 'json',
-	  type: 'GET',
-	}).done(function(response) {
+    //API call to obtain fixtures for a specified match day 
+    //This actually calls object and object runs functions
+  $.ajax({
+    headers: { 'X-Auth-Token': '183f8b1674a443d3b81e71fa06e8ac24' },
+    url: 'http://api.football-data.org/v1/competitions/426/fixtures?matchday=28',
+    dataType: 'json',
+    type: 'GET',
+  }).done(function(response) {
     console.log(response);
-	  fixtureGen.printMatches(response.fixtures);
-	});
+    fixtureGen.printMatches(response.fixtures);
+    userPicks.printPicks(response.fixtures);
+  });
 
   //API call to obtain the league table
-	$.ajax({
-	  headers: { 'X-Auth-Token': '183f8b1674a443d3b81e71fa06e8ac24' },
-	  url: 'http://api.football-data.org/v1/competitions/426/leagueTable',
-	  dataType: 'json',
-	  type: 'GET',
-	}).done(function(response) {
+  $.ajax({
+    headers: { 'X-Auth-Token': '183f8b1674a443d3b81e71fa06e8ac24' },
+    url: 'http://api.football-data.org/v1/competitions/426/leagueTable',
+    dataType: 'json',
+    type: 'GET',
+  }).done(function(response) {
     console.log(response);
-		tableGen.createStandings(response);
-	});
+    tableGen.createStandings(response);
+  });
 
 
 /*Competition # and corresponding league
